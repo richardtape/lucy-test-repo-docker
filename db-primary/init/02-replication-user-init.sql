a72dda10ddb742195c3b230a4139907753f55174
lucy-xueyi-xie <lucy.xie@ubc.ca>
2025-12-23 15:52:09 -0800

    refactored db replication user and passcode to .env file

--- db-primary/init/02-replication-user-init.sql
+++
@@ -1,3 +0,0 @@
--- allow replica user to be a replication client and receive all replication events for all db and tables
-CREATE USER IF NOT EXISTS 'replica'@'%' IDENTIFIED BY 'replica_pass';
-GRANT REPLICATION SLAVE ON *.* TO 'replica'@'%';
